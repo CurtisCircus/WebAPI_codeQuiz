a9064d238f78afb40979f93bfcc5b8a7910d504b
CurtisCircus <cakrieg63@gmail.com>
2023-09-12 18:51:42 -0400

    I have the first page of html set up so when the User goes to the web page they can read that the page is for taking a coding quiz and there's a button labeled 'start quiz'

---
+++ assets/JSCodeQuiz.js
@@ -0,0 +1,12 @@
+var startQuiz = document.getElementById("start");
+
+const questions = [
+    {
+      question: 'Which of these would have a "false" value',
+      choices: ['2 == two', '2 === 2', '"2" === 2'],
+      answer: '"2" === 2',
+    },
+    {
+
+    }
+  ];
